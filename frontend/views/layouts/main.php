fba5c61c0717f273b22c46c59e8cb5a2a6ee7564
Sergey Us <sergey.exu@gmail.com>
2016-08-12 09:12:14 +0000

    up

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -127,7 +127,10 @@ AppAsset::register($this);
         };
     </script>
 <?php $this->endBody() ?>
-<?php /*= GoogleAnalytics::widget(['trackingId'=> Yii::$app->settings->get('analitycs.gaTrackingId')]); */?> 
+<?= GoogleAnalytics::widget(['trackingId'=> Yii::$app->settings->get('analitycs.gaTrackingId')]); ?>
+
+<!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter39014535 = new Ya.Metrika({ id:39014535, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/39014535" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+
 </body>
 </html>
 <?php $this->endPage() ?>
